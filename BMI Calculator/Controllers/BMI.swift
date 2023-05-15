74f08ff1efd1f41af21bf076c067f8072e8cb20b
aparth443 <aparth443@gmail.com>
2023-05-15 16:04:30 +0530

    completed project

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by cumulations on 15/05/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
